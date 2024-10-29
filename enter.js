75dcfe6fb04e984ab2f1fd92cf90c45ed043414a
K_Minhyeok <61306657+Kim-Min-Hyeok@users.noreply.github.com>
2024-10-30 01:54:59 +0900

    sdf

--- enter.js
+++ enter.js
@@ -1,10 +1,5 @@
-// Firebase 구성 가져오기
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  push,
-} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+import { getDatabase, ref, update } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAWWeIHatcjbZJ_G4qTGE5AIib0uLkcJHc",
@@ -19,24 +14,39 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-// window 객체에 함수 추가
+// 팀 정보를 정의합니다.
+const teams = [
+  { name: "1조", members: ["최수원", "장주영", "김우현", "유수민", "김세현", "이수인"] },
+  { name: "2조", members: ["김지안", "강서영", "김수빈", "강신엽", "정민찬", "지석영", "김하진"] },
+  { name: "3조", members: ["김규리", "이채정", "김민준", "이유현", "김도원", "조성준", "김민규"] },
+  { name: "4조", members: ["김희민", "김하경", "박경민", "도현학", "최준영", "현승훈", "김나임"] },
+  { name: "5조", members: ["이지환", "김규희", "이서현", "김도경", "김지원", "김사랑", "김민제"] },
+];
+
 window.submitName = function submitName() {
   const nameInput = document.getElementById("participantName");
   const name = nameInput.value.trim();
   const messageElement = document.getElementById("message");
 
   if (name) {
-    // Firebase Database에 참가자 이름 추가
-    push(ref(database, "participants"), { name: name })
-      .then(() => {
-        messageElement.textContent = "이름이 성공적으로 등록되었습니다!";
-        nameInput.value = ""; // 입력 필드 비우기
-      })
-      .catch((error) => {
-        console.error("Error writing new participant to database", error);
-        messageElement.textContent =
-          "등록 중 오류가 발생했습니다. 다시 시도하세요.";
-      });
+    // 팀 배정 로직
+    const teamIndex = teams.findIndex((team) => team.members.includes(name));
+
+    if (teamIndex !== -1) {
+      // 해당 팀에 참가자 이름을 추가합니다.
+      const teamRef = ref(database, `teams/${teamIndex}/currentMembers`);
+      update(teamRef, { [Date.now()]: name })  // 고유 키 사용
+        .then(() => {
+          messageElement.textContent = `${teams[teamIndex].name}에 성공적으로 등록되었습니다!`;
+          nameInput.value = ""; // 입력 필드 비우기
+        })
+        .catch((error) => {
+          console.error("Error writing new participant to database", error);
+          messageElement.textContent = "등록 중 오류가 발생했습니다. 다시 시도하세요.";
+        });
+    } else {
+      messageElement.textContent = "입력한 이름이 팀 멤버 목록에 없습니다.";
+    }
   } else {
     messageElement.textContent = "이름을 입력하세요.";
   }
